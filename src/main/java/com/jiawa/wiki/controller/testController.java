6a6474b71677e0dd21d06a4a81b695d272b8a356
zhenshigang <zhenshigang@xdf.cn>
2023-06-30 15:24:22 +0800

    增加本地编译 以及测试@Value注解

--- src/main/java/com/jiawa/wiki/controller/testController.java
+++ src/main/java/com/jiawa/wiki/controller/testController.java
@@ -1,15 +1,17 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class testController {
     // RequestMapping既可以是post也可以是get
     // http有8种请求
+    @Value("${test.hello:TEST}")
+    private  String hello;
     @GetMapping("/test")
     public  String Test(){
-        return "hello world";
+        return "hello world" + hello;
     }
 }
